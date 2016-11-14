03280501e462d91b334e872c4c48e7306ca70f15
kevinchau321 <kevinchau321@gmail.com>
2016-11-13 19:14:44 -0800

    added template and loader packages

--- first_app/mysite/polls/views.py
+++ first_app/mysite/polls/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 from .models import Question
 
 # Create your views here.
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    tempalte = loader.get_template('polls/index.html')
+    template = loader.get_template('polls/index.html')
     context = {
         'latest_question_list': latest_question_list,
     }
