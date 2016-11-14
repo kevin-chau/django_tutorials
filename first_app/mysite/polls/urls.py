6768d3787bf3b393da069f6e17305f32af4c8d02
kevinchau321 <kevinchau321@gmail.com>
2016-11-13 18:55:04 -0800

    fixed detail regex urlpattern

--- first_app/mysite/polls/urls.py
+++ first_app/mysite/polls/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
 
     # ex: /polls/5
-    url(r'^(?P<question_id>[0-0]+)/$', views.detail, name = 'detail'),
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name = 'detail'),
 
     # ex: /polls/5/results/
     url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name = 'results'),
